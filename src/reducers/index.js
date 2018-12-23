74b8d449d36ea929ccbcad4ddd5b5f79731ad486
anthonyzhogolev <ale7496@gmail.com>
2018-12-23 17:06:27 +0500

    refactor: remove debug logs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,34 +8,34 @@ import labels from './labels';
 import itemLabels from './itemsLabels';
 
 export default combineReducers({
-   items, itemOrders, sortBy,filters,labels,itemLabels
+   items, itemOrders, sortBy, filters, labels, itemLabels
 });
 
-export const getSortedItems = (state) => {      
-   const filteredItems = fromItems.filterItems(state.items,state.filters);   
-   if(state.sortBy.columnName!==null){       
-      return fromItems.getColumnSortedItems(filteredItems,state.sortBy.columnName,state.sortBy.direction);
+export const getSortedItems = (state) => {
+   const filteredItems = fromItems.filterItems(state.items, state.filters);
+   if (state.sortBy.columnName !== null) {
+      return fromItems.getColumnSortedItems(filteredItems, state.sortBy.columnName, state.sortBy.direction);
    }
-   const itemOrders = state.itemOrders.filter((itemOrder)=>filteredItems.find((item)=>item.id===itemOrder.itemId));
+   const itemOrders = state.itemOrders.filter((itemOrder) => filteredItems.find((item) => item.id === itemOrder.itemId));
    return (fromItems.getDndSortedItems(filteredItems, itemOrders));
 }
 
 
 
- 
-export const getItems =(state)=>{
-   
+
+export const getItems = (state) => {
+
    const mainState = state;
-   
-   const items = getSortedItems(mainState).map((item)=>{
+
+   const items = getSortedItems(mainState).map((item) => {
       const itemLabels = mainState.labels.filter(
-         (label)=>(
+         (label) => (
             mainState.itemLabels.filter(
-               (itemLabel)=>(itemLabel.labelId==label.id && itemLabel.itemId==item.id)
-            ).length>0
+               (itemLabel) => (itemLabel.labelId == label.id && itemLabel.itemId == item.id)
+            ).length > 0
          )
       );
-      return Object.assign(item,{labels:itemLabels});
-   });   
+      return Object.assign(item, { labels: itemLabels });
+   });
    return items;
 }
