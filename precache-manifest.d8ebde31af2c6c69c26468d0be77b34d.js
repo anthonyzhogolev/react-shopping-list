1835fd9ae6573d4ee0d1cf3aab8ae3a8bcc61406
anthonyzhogolev <ale7496@gmail.com>
2018-12-23 17:50:00 +0500

    Updates

---
+++ precache-manifest.d8ebde31af2c6c69c26468d0be77b34d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-shopping-list/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "2a4d50d70c7bc24ad244",
+    "url": "/react-shopping-list/static/css/main.e1022c96.chunk.css"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-shopping-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7cdae43b176932446640",
+    "url": "/react-shopping-list/static/js/1.7cdae43b.chunk.js"
+  },
+  {
+    "revision": "00f771962ed094b49a2c",
+    "url": "/react-shopping-list/static/js/runtime~main.00f77196.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-shopping-list/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "2a4d50d70c7bc24ad244",
+    "url": "/react-shopping-list/static/js/main.2a4d50d7.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-shopping-list/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-shopping-list/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-shopping-list/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7cdae43b176932446640",
+    "url": "/react-shopping-list/static/css/1.ab3f0532.chunk.css"
+  },
+  {
+    "revision": "8c3c8138bbc3210748216c383ecc3c59",
+    "url": "/react-shopping-list/index.html"
+  }
+];
